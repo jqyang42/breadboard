ec390410440c511ed1e85f80753b8644f1a320c1
Jin Cho <jc695@duke.edu>
2021-04-16 11:45:31 -0400

    edits to logic.s

---
+++ logic.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+int paddle1_x;
+int paddle2_x;
+int paddle1_y;
+int paddle2_y;
+/* move paddle left */
+int move_up(){
+
+}
+
+/* move paddle up */
+
+/* move paddle right */
+
+/* move paddle down */
