aa30f3abda9749e9671a9a9cb5b2ed6ebf48d205
Jin Cho <jc695@duke.edu>
2021-04-20 18:23:19 -0400

    test wrapper

--- processor/Wrapper_tb.v
+++ processor/Wrapper_tb.v
@@ -156,7 +156,6 @@ module Wrapper_tb #(parameter FILE = "logic");
 	assign segRight_topBound = segRight_yRef - 40;
 	assign segRight_bottomBound = segRight_yRef + 40;
 
-
 	wire[2:0] winner;
 
 	regfile RegisterFile(.clock(clock), 
@@ -165,8 +164,7 @@ module Wrapper_tb #(parameter FILE = "logic");
 	.ctrl_readRegA(rs1), .ctrl_readRegB(rs2), 
 	.data_writeReg(rData), .data_readRegA(regA), .data_readRegB(regB), .posEdgeScreenEnd(screenEnd),
 	.winner(winner), .ball_x(ball_x), .ball_y(ball_y), .ball_xinit(ball_xinit), .ball_yinit(ball_yinit),
-	.p1_leftBound(p1_leftBound), .p1_rightBound(p1_rightBound), .p1_topBound(p1_topBound), .p1_bottomBound(p1_bottomBound), 
-	.p2_leftBound(p2_leftBound), .p2_rightBound(p2_rightBound), .p2_topBound(p2_topBound), .p2_bottomBound(p2_bottomBound),
+	.ball_xdir_factor(32'd1), .ball_ydir_factor(-32'd1),
 	.ball_xlim(ball_xlim), .ball_ylim(ball_ylim), .segLeft_topBound(segLeft_topBound), .segLeft_bottomBound(segLeft_bottomBound), 
 	.segRight_topBound(segRight_topBound), .segRight_bottomBound(segRight_bottomBound));
 						
