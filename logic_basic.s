1a5b2ee823989a95b2625535bf8dacfa1e58ce28
Jin Cho <jc695@duke.edu>
2021-04-22 03:38:31 -0400

    update stall logic. ball moves a little

--- logic_basic.s
+++ logic_basic.s
@@ -1,7 +1,7 @@
 initial_regs:
 addi	$r1, $r0, 1			# $r1 = $r0 + 1
 addi	$r2, $r0, -1			# $r2 = $r0 + -1
-addi    $r4, $r0, 2
+addi    $r4, $r0, 10
 addi    $r24, $r0, 12
 addi	$r25, $r0, 15			# $r25 = $r0 + 15
 addi    $r26, $r0, 1
@@ -22,14 +22,8 @@ add     $r16,   $r16,   $r28
 add     $r23,   $r16,   $r0     #writes the x to the reg that is directly connected to output of wrapper to use in VGAController
 and     $r28,   $r0,    $r28    #reset the math reg
 checking_stall:
-bne     $r3,    $r0,    wait_for_neg
+bne     $r3,    $r0,    checking_stall
 checking_stall_0:
-bne     $r3,    $r1,    wait_for_neg_1
+bne     $r3,    $r1,    checking_stall_0
 j		move_ball_x				# move again
-wait_for_neg:
-nop
-j   checking_stall
-wait_for_neg_1:
-nop
-j	checking_stall_0				# jump to checking_stall_0
 
